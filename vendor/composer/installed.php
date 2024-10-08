b307ba0b0794e29d50c4fe4aed7d2aaf8c4b83e4
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-10-08 10:06:16 -0500

    added ddev to project

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'drupal/recommended-project',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '06f734d57fc13ac0c657dfd1bc1757aa66a01924',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -254,9 +254,9 @@
             ),
         ),
         'drupal/recommended-project' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '06f734d57fc13ac0c657dfd1bc1757aa66a01924',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
